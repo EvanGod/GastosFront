2c63c9951dacc422640f9faac7c885e05f7f4292
EvanGod <2022171006@uteq.edu.mx>
2024-12-08 14:24:57 -0600

    first commit

---
+++ android/app/src/main/java/com/control/gastos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.control.gastos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.control.gastos',
   appName: 'ControlGastos',
   webDir: 'www'
 };
